fc0a4779a48dd599bbea4362ef8b482779e0568a
Lucas Nogueira <lucas@crabnebula.dev>
2023-11-05 08:38:26 -0300

    pnpm isnt really supported :( galactus

--- bindings/packager/nodejs/__test__/index.spec.mjs
+++ bindings/packager/nodejs/__test__/index.spec.mjs
@@ -1,12 +1,13 @@
 import test from 'ava'
 import process from 'process'
+import { execSync } from "child_process"
 
-import {
-  bundleApp
-} from '../build/index.js'
+import { bundleApp } from '../build/index.js'
 
 test('log error', async (t) => {
   process.env.CI = true
   process.chdir('../../../examples/electron')
+  execSync('yarn install')
+  await process.she
   t.is(await bundleApp(), undefined)
 })
