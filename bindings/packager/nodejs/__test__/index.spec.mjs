447954c3b6738d723b60aca6b5b90b4712ec402c
amr-crabnebula <amr@crabnebula.dev>
2023-11-23 20:12:21 +0200

    with yarn?

--- bindings/packager/nodejs/__test__/index.spec.mjs
+++ bindings/packager/nodejs/__test__/index.spec.mjs
@@ -1,22 +1,14 @@
-import test from "ava";
-import process from "process";
-import { execSync } from "child_process";
+import test from 'ava'
+import process from 'process'
+import { execSync } from "child_process"
 
-import { bundleApp } from "../build/index.js";
+import { bundleApp } from '../build/index.js'
 
-test("log error", async (t) => {
-  process.env.CI = true;
-  process.chdir("../../../examples/electron");
-  execSync("pnpm install");
-  t.is(
-    await bundleApp(
-      {
-        formats: process.env.PACKAGER_FORMATS
-          ? process.env.PACKAGER_FORMATS.split(",")
-          : null,
-      },
-      { verbosity: 2 },
-    ),
-    undefined,
-  );
-});
+test('log error', async (t) => {
+  process.env.CI = true
+  process.chdir('../../../examples/electron')
+  execSync('yarn install')
+  t.is(await bundleApp({
+    formats: process.env.PACKAGER_FORMATS ? process.env.PACKAGER_FORMATS.split(',') : null
+  }, { verbosity: 2 }), undefined)
+})
