e313e5351a53f50929557db0304665d1288218a8
amr-crabnebula <amr@crabnebula.dev>
2023-11-30 21:18:58 +0200

    I hate this

--- crates/updater/src/lib.rs
+++ crates/updater/src/lib.rs
@@ -786,7 +786,7 @@ impl Update {
 
         // if something went wrong during the extraction, we should restore previous app
         if let Err(e) = extract_archive(&mut archive, &self.extract_path) {
-            std::fs::remove_dir(self.extract_path)?;
+            std::fs::remove_dir(&self.extract_path)?;
             std::fs::rename(tmp_dir.path(), &self.extract_path)?;
             return Err(e);
         }
