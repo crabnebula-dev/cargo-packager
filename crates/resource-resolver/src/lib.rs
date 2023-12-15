4efec7509c94266c3a2a5aca84975d7e867e0d8f
wiiznokes <wiiznokes2@gmail.com>
2023-12-15 22:45:12 +0100

    fixCARGO_MANIFEST_DIR

--- crates/resource-resolver/src/lib.rs
+++ crates/resource-resolver/src/lib.rs
@@ -159,8 +159,7 @@ pub fn resource_dir_with_suffix(suffix: &str) -> Result<PathBuf> {
     }
 
     // when cargo run
-    let root_crate_dir =
-        env::var("CARGO_MANIFEST_DIR").expect("can't find CARGO_MANIFEST_DIR variable");
+    let root_crate_dir = env!("CARGO_MANIFEST_DIR");
     Ok(PathBuf::from(root_crate_dir).join(suffix))
 }
 
