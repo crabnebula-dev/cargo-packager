df28fc9daf7def4d44d6e606b91ff8c4f1508a57
Naman Garg <naman@crabnebula.dev>
2024-01-23 03:27:56 +0530

    Add PKGBUILD generator

--- crates/packager/src/package/pacman/mod.rs
+++ crates/packager/src/package/pacman/mod.rs
@@ -5,7 +5,7 @@
 use super::{Context, deb::{copy_custom_files, generate_data, tar_and_gzip_dir}};
 use heck::AsKebabCase;
 use sha2::{Digest, Sha512};
-use std::{fs::{self, File},
+use std::{fs::File,
     io::{self, Write},
     path::{Path, PathBuf},
 };
@@ -33,6 +33,7 @@ pub(crate) fn package(ctx: &Context) -> crate::Result<Vec<PathBuf>> {
 
     let pkg_dir = intermediates_path.join(&package_base_name);
     let pkg_path = config.out_dir().join(&package_name);
+    let pkgbuild_path = pkg_path.with_file_name("PKGBUILD");
 
     tracing::info!("Packaging {} ({})", package_name, pkg_path.display());
 
@@ -49,5 +50,92 @@ pub(crate) fn package(ctx: &Context) -> crate::Result<Vec<PathBuf>> {
     let data_tar_gz_path = tar_and_gzip_dir(pkg_dir)?;
     std::fs::copy(data_tar_gz_path, &pkg_path)?;
 
+    tracing::info!("Generating PKGBUILD: {}", pkgbuild_path.display());
+    generate_pkgbuild_file(config, arch, pkgbuild_path.as_path(), pkg_path.as_path())?;
+
     Ok(vec![pkg_path])
 }
+
+/// Generates the pacman PKGBUILD file.
+/// For more information about the format of this file, see
+/// https://wiki.archlinux.org/title/PKGBUILD
+fn generate_pkgbuild_file(
+    config: &Config,
+    arch: &str,
+    dest_dir: &Path,
+    package_path: &Path,
+  ) -> crate::Result<()> {
+    let pkgbuild_path = dest_dir.with_file_name("PKGBUILD");
+    let mut file = util::create_file(&pkgbuild_path)?;
+  
+    if let Some(authors) = &config.authors {
+      writeln!(file, "# Maintainer: {}", authors.join(", "))?;
+    }
+    writeln!(file, "pkgname={}-bin", AsKebabCase(&config.product_name))?;
+    writeln!(file, "pkgver={}", config.version)?;
+    writeln!(file, "pkgrel=1")?;
+    writeln!(file, "epoch=")?;
+    writeln!(file, "pkgdesc=\"{}\"", config.description.as_deref().unwrap_or(""))?;
+    writeln!(file, "arch=('{}')", arch)?;
+
+    if let Some(homepage) = &config.homepage {
+      writeln!(file, "url=\"{}\"", homepage)?;
+    }
+  
+    let dependencies = config
+      .pacman()
+      .cloned()
+      .and_then(|d| d.depends)
+      .unwrap_or_default();
+    writeln!(file, "depends=({})", dependencies.join(" \n"))?;
+  
+    let provides = config
+      .pacman()
+      .cloned()
+      .and_then(|d| d.provides)
+      .unwrap_or_default();
+    writeln!(file, "provides=({})", provides.join(" \n"))?;
+  
+    let conflicts = config
+      .pacman()
+      .cloned()
+      .and_then(|d| d.conflicts)
+      .unwrap_or_default();
+    writeln!(file, "conflicts=({})", conflicts.join(" \n"))?;
+  
+    let replaces = config
+      .pacman()
+      .cloned()
+      .and_then(|d| d.replaces)
+      .unwrap_or_default();
+    writeln!(file, "replaces=({})", replaces.join(" \n"))?;
+  
+    writeln!(file, "options=(!lto)")?;
+    let source = config
+        .pacman()
+        .cloned()
+        .and_then(|d| d.source)
+        .unwrap_or_default();
+    
+    if source.is_empty() {
+      writeln!(file, "source=({:?})", package_path.file_name().unwrap())?;
+    } else {
+      writeln!(file, "source=({})", source.join(" \n"))?;
+    }
+  
+    // Generate SHA512 sum of the package
+    let mut sha_file = File::open(package_path)?;
+    let mut sha512 = Sha512::new();
+    io::copy(&mut sha_file, &mut sha512)?;
+    let sha_hash = sha512.finalize();
+  
+    writeln!(file, "sha512sums=(\"{:x}\")", sha_hash)?;
+    writeln!(
+      file,
+      "package() {{\n\tcp -r ${{srcdir}}/data/* ${{pkgdir}}/\n}}"
+    )?;
+  
+    file.flush()?;
+    Ok(())
+  }
+  
