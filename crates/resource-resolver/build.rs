93f0354b43f816c28cb01407569c2cefd3c61001
Amr Bashir <amr@crabnebula.dev>
2024-09-05 17:02:54 +0300

    rustc-check-cfg

--- crates/resource-resolver/build.rs
+++ crates/resource-resolver/build.rs
@@ -4,6 +4,7 @@ fn main() {
     if env::var("CARGO_FEATURE_AUTO_DETECT_FORMAT").is_ok() {
         println!("cargo:rerun-if-env-changed=CARGO_PACKAGER_FORMAT");
         if let Ok(var) = env::var("CARGO_PACKAGER_FORMAT") {
+            println!("cargo:rustc-check-cfg=CARGO_PACKAGER_FORMAT=\"{}\"", var);
             println!("cargo:rustc-cfg=CARGO_PACKAGER_FORMAT=\"{}\"", var);
         }
     }
