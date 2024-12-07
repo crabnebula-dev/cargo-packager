d1be4bc39a03565c44dcf76748eab8bc8459b076
Manuel Abeledo <manuel.angel@abeledochang.com>
2024-12-06 18:16:20 -0600

    fix arch conditional compilation

--- crates/packager/src/package/app/mod.rs
+++ crates/packager/src/package/app/mod.rs
@@ -488,7 +488,6 @@ fn remove_extra_attr(app_bundle_path: &Path) -> crate::Result<()> {
 }
 
 // Copies the embedded.provisionprofile file to the Contents directory, if needed.
-#[tracing::instrument(level = "trace", skip(config))]
 #[cfg(target_os = "macos")]
 fn copy_embedded_provisionprofile_file(
     contents_directory: &Path,
