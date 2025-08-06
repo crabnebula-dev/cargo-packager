b337564c0e5a9de966b4124890dddea1e353acb4
Fabian-Lars <118197967+FabianLars-crabnebula@users.noreply.github.com>
2025-08-06 14:38:30 +0200

    fix(linux): pull latest linuxdeploy appimage plugin (#362)
    * fix(linux): pull latest linuxdeploy appimage plugin
    
    * fix file name
    
    * clippy
    
    * force binstall dioxus-cli
    
    * forcefully binstall tauri-cli as well (wtf)
    
    * allow cdla - ref https://github.com/rustls/webpki-roots/commit/90c48f38672f4300eea5f1b712d4ccca8ad82123
    
    * time for me to go to sleep i guess

---
+++ .changes/update-appimage-plugin.md
@@ -0,0 +1,6 @@
+---
+"cargo-packager": patch
+"@crabnebula/packager": patch
+---
+
+Updated linuxdeploy's AppImage plugin to not require libfuse on the user's system anymore.
--- crates/packager/src/package/appimage/mod.rs
+++ crates/packager/src/package/appimage/mod.rs
@@ -32,6 +32,11 @@ fn donwload_dependencies(
             format!("linuxdeploy-{linuxdeploy_arch}.AppImage"),
             format!("https://github.com/tauri-apps/binary-releases/releases/download/linuxdeploy/linuxdeploy-{linuxdeploy_arch}.AppImage")
         ),
+        // This path is incompatible with cross-platform compilation but linuxdeploy doens't support that anyway.
+        (
+            "linuxdeploy-plugin-appimage.AppImage".to_string(),
+            format!("https://github.com/linuxdeploy/linuxdeploy-plugin-appimage/releases/download/continuous/linuxdeploy-plugin-appimage-{arch}.AppImage")
+        ),
     ];
 
     let user_deps = ctx
